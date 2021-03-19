6ebd45bf9eda3f399b3bbefbe68b6e5eb79be349
Ilyas Ilyasov <ilyas.ilyasov.1@gmail.com>
2021-03-19 15:50:45 +0400

    simpple api example

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ ENV PYTHONUNBUFFERED=1
 WORKDIR /code
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
+RUN pip3 install djangorestframework
 COPY . /code/
--- docker_django_example/settings.py
+++ docker_django_example/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'myapi.apps.MyapiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
